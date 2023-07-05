5e341afb0a5355ec1a0cdc38addb77402ed8cc8d
FavourPeters <favourpeters1@gmail.com>
2023-07-05 04:07:10 +0100

    updates

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 // UI
 const nav = document.querySelector('#navi');
 const list = document.querySelector('.list');
+const closeNav = document.querySelector('#close');
 
 
-
+// Show Navigation
 nav.addEventListener('click', function () {
     list.classList.remove('-translate-x-full');
 });
 
+//Remove Navigation
 list.addEventListener('mouseleave', rem_nav)
+closeNav.addEventListener('click', rem_nav)
 
 function rem_nav() {
     list.classList.add('-translate-x-full');
