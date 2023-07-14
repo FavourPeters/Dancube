4369aed0dc960a1280cbf81d3d0616687b5d38d1
FavourPeters <favourpeters1@gmail.com>
2023-07-14 09:13:01 +0100

    Register

--- src/register.js
+++ src/register.js
@@ -5,12 +5,21 @@ const signInB = document.querySelector('.sign-in_btn');
 const signUpB = document.querySelector('.sign-up_btn');
 
 // Events
-signUpB.addEventListener('click', inside);
+signUpB.addEventListener('click', sIn);
+signInB.addEventListener('click', sOut);
 
-
-function inside(e){
+// Functions
+// Sign up
+function sIn(e){
     signIn.className += ' hidden';
     signUp.classList.remove('hidden');
+
+    e.preventDefault();
+}
+// Sign up
+function sOut(e){
+    signUp.className += ' hidden';
+    signIn.classList.remove('hidden');
 
     e.preventDefault();
 }
